6c5d582f879a18097b852027d4c91cc26b9e0e9e
Lucas Galvao <lucasmelo_galvao@hotmail.com>
2022-03-03 23:50:18 -0300

    starting challenge

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,51 @@
+import { useState, useEffect } from "react";
+import { MovieCard } from "./MovieCard";
+
+import "../styles/content.scss";
+
+interface GenreResponseProps {
+  id: number;
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  title: string;
+}
+
+interface MovieProps {
+  imdbID: string;
+  Title: string;
+  Poster: string;
+  Ratings: Array<{
+    Source: string;
+    Value: string;
+  }>;
+  Runtime: string;
+}
+
+
 export function Content() {
   // Complete aqui
+  const [selectedGenreId, setSelectedGenreId] = useState(1);
+  const [selectedGenre, setSelectedGenre] = useState<GenreResponseProps>({} as GenreResponseProps);
+  const [genres, setGenres] = useState<GenreResponseProps[]>([]);
+  const [movies, setMovies] = useState<MovieProps[]>([]);
+  
+  return (
+    <div style={{ display: 'flex', flexDirection: 'row' }}>
+      
+      {/* Content */}
+      <div className="container">
+        <header>
+          <span className="category">Categoria:<span> {selectedGenre.title}</span></span>
+        </header>
+
+        <main>
+          <div className="movies-list">
+            {movies.map(movie => (
+              <MovieCard key ={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+            ))}
+          </div>
+        </main>
+      </div>
+    </div>
+  );
+
 }
